eab0b95b6065b363a9a9353ee066591bfc038814
Park-Jingyeong <pjg0305pjg@gmail.com>
2024-06-07 22:04:33 +0900

    feat: 중고마켓 페이지 화면 구성 - 미완성

--- src/pages/MarketPage/components/AllItemsSection.jsx
+++ src/pages/MarketPage/components/AllItemsSection.jsx
@@ -3,6 +3,7 @@ import ItemCard from "./ItemCard";
 import PaginationBar from "../../../components/UI/PaginationBar";
 import { getProducts } from "../../../api/itemApi";
 import { useState, useEffect } from "react";
+import "./AllItemsSection.css";
 
 const AllItemsSection = () => {
   const [itemList, setItmeList] = useState([]);
@@ -27,20 +28,25 @@ const AllItemsSection = () => {
   }, [orderBy]);
 
   return (
-    <div>
-      <div>
-        <h1>전체 상품</h1>
-        <div>
-          <input />
-          <Link to="additem">상품 등록하기</Link>
+    <div className="allItems">
+      <div className="allItemsBar">
+        <h1 className="allItemsTitle">전체 상품</h1>
+        <div className="allItmesBarRight">
+          <input
+            className="searchItem"
+            placeholder="검색할 상품을 입력해주세요"
+          />
+          <Link className="addItem" to="additem">
+            상품 등록하기
+          </Link>
           <div>
             <button onClick={handleRecentClick}>최신순</button>
             <button onClick={handleFavoriteClick}>좋아요순</button>
           </div>
         </div>
       </div>
-      <div>
-        {sortedItemList.map((item) => (
+      <div className="itemCards">
+        {sortedItemList?.map((item) => (
           <ItemCard item={item} key={item.id} />
         ))}
       </div>
